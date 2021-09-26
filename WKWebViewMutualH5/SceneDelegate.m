b70e4a91b852b2843a9e37f584a37f3d1552796b
ITHanYong <1152829171@qq.com>
2021-09-26 22:17:14 +0800

    H5 IOS

--- WKWebViewMutualH5/SceneDelegate.m
+++ WKWebViewMutualH5/SceneDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "SceneDelegate.h"
+#import "ViewController.h"
 
 @interface SceneDelegate ()
 
@@ -18,6 +19,16 @@
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    
+    ViewController *rootVC = [[ViewController alloc] init];
+    UINavigationController *nvc = [[UINavigationController alloc] initWithRootViewController:rootVC];
+    UIWindowScene *windowScene = (UIWindowScene *)scene;
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window = [[UIWindow alloc] initWithWindowScene:windowScene];
+    [self.window setWindowScene:windowScene];
+    [self.window setBackgroundColor:[UIColor whiteColor]];
+    [self.window setRootViewController:nvc];
+    [self.window makeKeyAndVisible];
 }
 
 
